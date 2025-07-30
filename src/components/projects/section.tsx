f8c15a6d90450874d624921e21392979ab16475b
juandavid015 <juandavidgr1002@gmail.com>
2025-07-30 14:06:36 -0500

    feat: enhance ProjectsSection to include dynamic CTAs and subtitles based on project index for improved user engagement

--- src/components/projects/section.tsx
+++ src/components/projects/section.tsx
@@ -14,10 +14,41 @@ interface ProjectsSectionProps {
   className?: string;
 }
 
-
 const ProjectsSection = ({ className }: ProjectsSectionProps) => {
   const t = useTranslations("projects");
-  const project = data.projects;
+  const project = data.projects.map((project, idx) => {
+    return {
+      ...project,
+      CTA:
+        idx === 0
+          ? {
+              text: t("items.first.CTA.text"),
+              link: t("items.first.CTA.link"),
+            }
+          : idx === 1
+          ? {
+              text: t("items.second.CTA.text"),
+              link: t("items.second.CTA.link"),
+            }
+          : idx === 1
+          ? {
+              text: t("items.third.CTA.text"),
+              link: t("items.third.CTA.link"),
+            }
+          : {
+              text: "",
+              link: "",
+            },
+      subtitle:
+        idx === 0
+          ? t("items.first.subtitle")
+          : idx === 1
+          ? t("items.second.subtitle")
+          : idx === 2
+          ? t("items.third.subtitle")
+          : "",
+    };
+  });
 
   return (
     <Section title={t("title")} className={className} id="projects">
