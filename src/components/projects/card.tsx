cd488e88964a669cf2cb2d305e379af60b3c853c
juandavid015 <juandavidgr1002@gmail.com>
2025-07-30 00:27:13 -0500

    feat: implement ProjectCard and ProjectsSection components for project display with carousel functionality

---
+++ src/components/projects/card.tsx
@@ -0,0 +1,85 @@
+import { cn } from "@/lib/utils";
+import { ArrowUpRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ProjectHeaderCardProps {
+  imageSrc: string;
+  title: string;
+}
+
+export const ProjectHeaderCard = ({
+  imageSrc,
+  title,
+}: ProjectHeaderCardProps) => {
+  return (
+    <div className="w-full h-32 flex items-center justify-center">
+      <Image
+        width={50}
+        height={50}
+        className="rounded-full border border-gray-950 object-cover overflow-hidden"
+        alt={`image ${title}`}
+        src={imageSrc}
+      />
+    </div>
+  );
+};
+
+interface ProjectContentProps {
+  title: string;
+  subtitle: string;
+  cta: {
+    text: string;
+    link: string;
+  };
+}
+
+export const ProjectContent = ({
+  title,
+  subtitle,
+  cta,
+}: ProjectContentProps) => {
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="flex flex-col">
+        <Link
+          href={cta.link}
+          target="_blank"
+          className="font-light underline line-clamp-1"
+        >
+          {subtitle}
+        </Link>
+        <h3 className="uppercase line-clamp-1">{title}</h3>
+      </div>
+      <Link
+        href={cta.link}
+        target="_blank"
+        className={cn(
+          "w-full border border-gray-950 p-3 uppercase hover:bg-indigo-700 duration-300",
+          "flex justify-center items-center gap-1.5 hover:text-white"
+        )}
+      >
+        {cta.text}
+        <ArrowUpRight className="w-5 h-5" />
+      </Link>
+    </div>
+  );
+};
+
+interface ProjectCardProps
+  extends ProjectHeaderCardProps,
+    ProjectContentProps {}
+
+export const ProjectCard = ({
+  title,
+  subtitle,
+  imageSrc,
+  cta,
+}: ProjectCardProps) => {
+  return (
+    <div className="flex flex-col max-w-xs">
+      <ProjectHeaderCard title={title} imageSrc={imageSrc} />
+      <ProjectContent title={title} subtitle={subtitle} cta={cta} />
+    </div>
+  );
+};
---
+++ src/components/projects/section.tsx
@@ -0,0 +1,49 @@
+import { useTranslations } from "next-intl";
+import Section from "../section";
+import data from "@/data.json";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+import { ProjectCard } from "./card";
+
+interface ProjectsSectionProps {
+  className?: string;
+}
+
+
+const ProjectsSection = ({ className }: ProjectsSectionProps) => {
+  const t = useTranslations("projects");
+  const project = data.projects;
+
+  return (
+    <Section title={t("title")} className={className}>
+      <Carousel className="w-full">
+        <CarouselContent>
+          {project.map((project) => {
+            return (
+              <CarouselItem
+                key={`exp-${project.title}`}
+                className="w-fit max-w-sm w-full"
+              >
+                <ProjectCard
+                  title={project.title}
+                  imageSrc={project.imageSrc}
+                  subtitle={project.subtitle}
+                  cta={project.CTA}
+                />
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </Section>
+  );
+};
+
+export default ProjectsSection;
